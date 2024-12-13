e24dd48e4c31a1e160d3aab66a2c7d9b38373867
naufal aufa <muhammadnaufalaufarifqi@gmail.com>
2024-12-13 13:11:30 +0700

    fix responsible breakpoints

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -22,7 +22,10 @@ const Product = ({
   );
 
   return (
-    <div id="product" className="py-12 w-screen max-w-full px-8">
+    <div
+      id="product"
+      className="py-12 w-screen m-auto grid place-content-center place-items-center max-w-full px-8"
+    >
       <h1 className="text-center font-serif">Furniture Product</h1>
       <h1 className="text-center text-3xl sm:text-5xl font-serif">
         Furniture Product
